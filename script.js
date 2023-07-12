f3f8d5e9e122db8806d29624f3d42bfe4e15cb9f
FlavioNeri00 <flavio.neri00@gmail.com>
2023-07-12 18:02:22 +0200

    W3-D3-U1

---
+++ script.js
@@ -0,0 +1,32 @@
+let newTasks = [];
+
+const taskMaker = (taskEvent) => {
+  const parentTask = taskEvent.target;
+  console.log(parentTask);
+  const task = parentTask.elements;
+  console.log(task);
+  taskEvent.preventDefault();
+  for (i = 0; i < task.length; i++) {
+    if (task[i].id !== "BUTTON") {
+      newTasks.push(task[i].value);
+      // newTask = { "nome": task[i].value};
+      const list = document.querySelector("ul");
+      list.innerHTML +=
+        `<li id= "${newTasks[newTasks.length - 1]}" > ${newTasks[newTasks.length - 1]} ` +
+        `<button onclick="myFunction(id)" id="${newTasks[newTasks.length - 1]}"> Reset </button>`;
+      task[i].value = "";
+      const listItems = document.querySelectorAll("ul li");
+    }
+  }
+};
+
+function myFunction(id) {
+  var oRow = document.getElementById(id);
+  oRow.remove();
+  let s = newTasks.findIndex((a) => a === id);
+  console.log(s);
+  newTasks.splice(s);
+  console.log(newTasks);
+}
+
+taskMaker();
